8ccdcad424103ec0b67e662eaea153452512eb56
Colorful Invisible <realismofantastico@gmail.com>
2024-07-11 02:23:17 +0200

    pixel-manipulation success

--- dist/index.0fbc91cd.js
+++ dist/index.0fbc91cd.js
@@ -593,49 +593,71 @@ var _utils = require("./utils");
 new (0, _p5Default.default)((sk)=>{
     let animalVideo;
     let videoDimensions;
+    let cellSize = 10;
     let defaultDensity;
-    let videoReady = false;
     sk.preload = ()=>{
         animalVideo = sk.createVideo((0, _beetle02Mp4Default.default));
-        animalVideo.elt.muted = true; // Mute the video
-        animalVideo.elt.playsInline = true; // Allow inline playback on iOS
+        animalVideo.elt.muted = true;
+        animalVideo.elt.playsInline = true;
     };
-    function attemptAutoplay() {
-        let playPromise = animalVideo.elt.play();
-        if (playPromise !== undefined) playPromise.then((_)=>{
-            // Autoplay started
-            animalVideo.loop();
-        }).catch((error)=>{
-            // Autoplay was prevented
-            console.warn("Autoplay was prevented:", error);
-        // You might want to add a subtle play button here as a fallback
-        });
-    }
     sk.setup = ()=>{
         defaultDensity = sk.displayDensity();
-        sk.pixelDensity(defaultDensity);
         sk.createCanvas(sk.windowWidth, sk.windowHeight);
         animalVideo.hide();
+        animalVideo.elt.willReadFrequently = true;
         animalVideo.elt.addEventListener("loadedmetadata", ()=>{
             videoDimensions = (0, _utils.calculateVideoDimensions)(sk, animalVideo);
-            videoReady = true;
-            attemptAutoplay();
+            animalVideo.loop();
         });
+        animalVideo.show = false;
     };
     sk.draw = ()=>{
-        sk.background(0);
-        if (videoDimensions && videoReady) sk.image(animalVideo, videoDimensions.x, videoDimensions.y, videoDimensions.w, videoDimensions.h);
+        // sk.background("aqua");
+        sk.background(255, 255, 255, 120);
+        if (videoDimensions) {
+            animalVideo.loadPixels();
+            for(let y = 0; y < videoDimensions.h; y += cellSize)for(let x = 0; x < videoDimensions.w; x += cellSize){
+                // Map canvas coordinates to video coordinates
+                let videoX = sk.map(x, 0, videoDimensions.w, 0, animalVideo.width);
+                let videoY = sk.map(y, 0, videoDimensions.h, 0, animalVideo.height);
+                let index = (sk.floor(videoX) + sk.floor(videoY) * animalVideo.width) * 4;
+                let r = animalVideo.pixels[index + 0];
+                let g = animalVideo.pixels[index + 1];
+                let b = animalVideo.pixels[index + 2];
+                let brightness = (r + g + b) / 3;
+                let posX = videoDimensions.x + x + cellSize / 2;
+                let posY = videoDimensions.y + y + cellSize / 2;
+                let colorFill;
+                let colorStroke;
+                if (brightness < 20) {
+                    colorFill = sk.color(0, 0, 0, 0);
+                    colorStroke = sk.color(0, 0, 0, 0);
+                } else if (brightness >= 20 && brightness < 120) {
+                    colorFill = sk.color("tomato");
+                    colorStroke = sk.color(0, 0, 0, 255);
+                } else if (brightness >= 120 && brightness < 160) {
+                    colorFill = sk.color("aquamarine");
+                    colorStroke = sk.color(0, 0, 0, 0);
+                } else {
+                    colorFill = sk.color("yellow");
+                    colorStroke = sk.color(0, 0, 0, 255);
+                }
+                sk.push();
+                sk.fill(colorFill);
+                sk.stroke(colorStroke);
+                sk.ellipse(posX, posY, cellSize - 2, cellSize - 2);
+                sk.pop();
+            }
+        }
+        if (animalVideo.show) sk.image(animalVideo, videoDimensions.x, videoDimensions.y, videoDimensions.w, videoDimensions.h);
     };
     sk.windowResized = ()=>{
         sk.resizeCanvas(sk.windowWidth, sk.windowHeight);
         if (animalVideo.width) videoDimensions = (0, _utils.calculateVideoDimensions)(sk, animalVideo);
     };
     sk.keyPressed = ()=>{
-        if (sk.key === "s" || sk.key === "S") (0, _utils.saveSnapshot)(sk, defaultDensity, 2, "sketch");
-    };
-    // Add this to attempt autoplay on any user interaction with the page
-    sk.mousePressed = sk.touchStarted = ()=>{
-        if (!animalVideo.elt.playing) attemptAutoplay();
+        if (sk.key === "s" || sk.key === "S") (0, _utils.saveSnapshot)(sk, defaultDensity, 4);
+        if (sk.key === "h") animalVideo.show = !animalVideo.show;
     };
 });
 
@@ -32698,11 +32720,11 @@ parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "calculateVideoDimensions", ()=>calculateVideoDimensions);
 parcelHelpers.export(exports, "saveSnapshot", ()=>saveSnapshot);
 let countSaved = 1;
-function saveSnapshot(sk, defaultDensity, densityFactor = 2, filename = "sketch") {
+function saveSnapshot(sk, defaultDensity, densityFactor = 2) {
     const currentDensity = sk.pixelDensity();
     sk.pixelDensity(defaultDensity * densityFactor);
     sk.redraw();
-    sk.saveCanvas(`${filename}${countSaved}`, "png");
+    sk.saveCanvas(`sketch_${countSaved}`, "png");
     countSaved++;
     sk.pixelDensity(currentDensity);
     sk.redraw();
--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,16 +1,11 @@
 // ---- SAVE P5 CANVAS SNAPSHOT AS PNG
 // -----------------------------------
 let countSaved = 1;
-function saveSnapshot(
-  sk,
-  defaultDensity,
-  densityFactor = 2,
-  filename = "sketch"
-) {
+function saveSnapshot(sk, defaultDensity, densityFactor = 2) {
   const currentDensity = sk.pixelDensity();
   sk.pixelDensity(defaultDensity * densityFactor);
   sk.redraw();
-  sk.saveCanvas(`${filename}${countSaved}`, "png");
+  sk.saveCanvas(`sketch_${countSaved}`, "png");
   countSaved++;
   sk.pixelDensity(currentDensity);
   sk.redraw();
